22bd78018750607887bc3f247bf646a3258bccd9
Duc Tran <tblyduc2412@gmail.com>
2023-03-16 14:04:10 +0700

    Define the checkout form model

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CartService } from '../cart.service';
 import { Product } from '../products';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-cart',
@@ -9,10 +10,25 @@ import { Product } from '../products';
 })
 export class CartComponent {
   private cartService: CartService;
+  private formBuilder: FormBuilder;
   public items: Product[];
+  public checkoutForm;
 
-  constructor(cartService: CartService) {
+  constructor(cartService: CartService, formBuilder: FormBuilder) {
     this.cartService = cartService;
+    this.formBuilder = formBuilder;
+
     this.items = this.cartService.getItems();
+    this.checkoutForm = this.formBuilder.group({
+      name: '',
+      address: '',
+    });
+  }
+
+  onSubmit(): void {
+    // process checkout data here
+    this.items = this.cartService.clearCart();
+    console.warn('Your order has been submitted', this.checkoutForm.value);
+    this.checkoutForm.reset();
   }
 }
