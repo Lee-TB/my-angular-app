8e4fd81235ae480401dc5a49c3900055f9cf4627
Duc Tran <tblyduc2412@gmail.com>
2023-03-16 14:24:51 +0700

    create checkout form template.

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -27,6 +27,7 @@ export class CartComponent {
 
   onSubmit(): void {
     // process checkout data here
+
     this.items = this.cartService.clearCart();
     console.warn('Your order has been submitted', this.checkoutForm.value);
     this.checkoutForm.reset();
